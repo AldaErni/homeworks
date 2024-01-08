80ed08cd5259ec90caa6186f5becb033b150f200
AldaErni <75014923+AldaErni@users.noreply.github.com>
2024-01-08 15:16:25 +0600

    Add files via upload

---
+++ js-76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Aldazhar'
+const lastName = 'Erniyaz'
+let height = 185
+let age = 26
+const nationality = 'Kazakh'
+
+console.log(firstName, lastName)
+console.log('age:', age)
+console.log('height:', height)
+console.log('nationality:', nationality)
