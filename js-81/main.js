e4c11f5f2551b5749e06dda78c15003fd5e1963d
AldaErni <75014923+AldaErni@users.noreply.github.com>
2024-01-09 14:04:02 +0600

    Add files via upload

---
+++ js-81/main.js
@@ -0,0 +1,13 @@
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+// for (let i=1; i<numbers.length;i=i+2){
+//   console.log(numbers[i])
+// }
+
+
+const rainbow = ['Red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+for (let i=6; i>=0; i--){
+  console.log(rainbow[i])
+}
